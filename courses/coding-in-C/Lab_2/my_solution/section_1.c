c559a3fde4327da34a7333dfe3b6213b34ba8eef
K Müller <muellerk2006@gmail.com>
2026-01-30 14:53:47 +0100

    Implement Section 1 task

--- courses/coding-in-C/Lab_2/my_solution/section_1.c
+++ courses/coding-in-C/Lab_2/my_solution/section_1.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
 void print_chessboard() {
+    for (int file = 8; file >= 1; file--) {
+        for (int rank = 0; rank < 8; rank++) {
+            char letter = 'A' + rank;
+            printf("%c%d ", letter, file);
+        }
 
+        printf("\n");
+    }
 }
 
 int main() {
