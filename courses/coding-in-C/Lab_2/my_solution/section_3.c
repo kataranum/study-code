ad67ba299b1b76dde691a7352e1c7535deaec397
K Müller <muellerk2006@gmail.com>
2026-02-01 13:51:52 +0100

    Print column letters in first row

--- courses/coding-in-C/Lab_2/my_solution/section_3.c
+++ courses/coding-in-C/Lab_2/my_solution/section_3.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 
+void print_column_letters() {
+    printf("  ");
+
+    for (int column = 0; column < 8; column++) {
+        char letter = 'A' + column;
+        printf(" %c ", letter);
+    }
+
+    printf("\n");
+}
+
+void print_chessboard() {
+    print_column_letters();
+}
+
 int main() {
-    printf("hello\n");
+    print_chessboard();
 
     return 0;
 }
