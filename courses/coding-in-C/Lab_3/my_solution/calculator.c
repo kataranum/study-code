c46becd9020b5688d2cdbd8a5cf329903dea1b25
K Müller <muellerk2006@gmail.com>
2026-02-04 09:07:03 +0100

    Create necesary functions

--- courses/coding-in-C/Lab_3/my_solution/calculator.c
+++ courses/coding-in-C/Lab_3/my_solution/calculator.c
@@ -1,5 +1,22 @@
 #include <stdio.h>
 
+int get_input_values(float* lhs, float* rhs);
+int get_operation(char* operation);
+float perform_calculation(float lhs, float rhs, char operation);
+
 int main() {
     return 0;
+}
+
+int get_input_values(float* lhs, float* rhs) {
+    return -1;
+}
+
+int get_operation(char* operation) {
+    return -1;
+}
+
+float perform_calculation(float lhs, float rhs, char operation) {
+    // TODO
+    return 0.0;
 }
