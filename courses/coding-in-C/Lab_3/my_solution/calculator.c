aaf8ce21f458b94205e6247e713ed8e94ba05335
K Müller <muellerk2006@gmail.com>
2026-02-04 09:11:49 +0100

    Implement get_input_values

--- courses/coding-in-C/Lab_3/my_solution/calculator.c
+++ courses/coding-in-C/Lab_3/my_solution/calculator.c
@@ -5,11 +5,45 @@ int get_operation(char* operation);
 float perform_calculation(float lhs, float rhs, char operation);
 
 int main() {
+    printf("Calculator program (will run only once :P)\n");
+
+    float lhs = 0.0;
+    float rhs = 0.0;
+    if (! get_input_values(&lhs, &rhs)) {
+        printf("Could not read input values.\n");
+        return -1;
+    }
+
+    char operation = '\0';
+    if (! get_operation(&operation)) {
+        printf("Could not read operation.\n");
+        return -1;
+    }
+
+    float result = perform_calculation(lhs, rhs, operation);
+    printf("Result: %f\n", result);
+
     return 0;
 }
 
 int get_input_values(float* lhs, float* rhs) {
-    return -1;
+    float input_lhs = 0.0;
+    float input_rhs = 0.0;
+
+    printf("Input first number: ");
+    scanf("%f", &input_lhs);
+
+    // TODO: Check validity
+
+    printf("Input second number: ");
+    scanf("%f", &input_rhs);
+
+    // TODO: Check validity
+
+    *lhs = input_lhs;
+    *rhs = input_rhs;
+
+    return 0;
 }
 
 int get_operation(char* operation) {
