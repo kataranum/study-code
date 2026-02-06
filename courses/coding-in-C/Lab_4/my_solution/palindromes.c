af25f2ecf9bdf12377bfda0813a31feb2f386bc5
K Müller <muellerk2006@gmail.com>
2026-02-06 14:29:31 +0100

    Read user input

--- courses/coding-in-C/Lab_4/my_solution/palindromes.c
+++ courses/coding-in-C/Lab_4/my_solution/palindromes.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+bool read_word(char* str, int max_len) {
+    bool result = fgets(str, max_len, stdin);
+
+    int last_char_index = strlen(str) - 1;
+    str[last_char_index] = '\0';
+
+    return result;
+}
 
 int main() {
+    printf("Enter a word: ");
+
+    char buffer[100];
+    if (! read_word(buffer, sizeof(buffer))) {
+        printf("Failed to read word.\n");
+    }
+
     return 0;
 }
