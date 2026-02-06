3322609da635f657bc063894d073c9ced1ad3fca
K Müller <muellerk2006@gmail.com>
2026-02-06 14:34:10 +0100

    Implement case-sensitive palindrome check

--- courses/coding-in-C/Lab_4/my_solution/palindromes.c
+++ courses/coding-in-C/Lab_4/my_solution/palindromes.c
@@ -11,12 +11,34 @@ bool read_word(char* str, int max_len) {
     return result;
 }
 
+bool is_palindrome_case_sensitive(char* str) {
+    int str_len = strlen(str);
+
+    for (int i = 0; i < str_len; i++) {
+        char* p_front = str + i;
+        char* p_back = str + (str_len - i - 1);
+
+        if (*p_front != *p_back) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
 int main() {
     printf("Enter a word: ");
 
     char buffer[100];
     if (! read_word(buffer, sizeof(buffer))) {
         printf("Failed to read word.\n");
+    }
+
+    if (is_palindrome_case_sensitive(buffer)) {
+        printf("Your word \"%s\" IS a palindrome!\n", buffer);
+    }
+    else {
+        printf("Your word \"%s\" IS NOT a palindrome!\n", buffer);
     }
 
     return 0;
