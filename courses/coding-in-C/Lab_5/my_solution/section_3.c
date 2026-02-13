3c955e32c38934b175c2d82171cc2b47b33f23c2
K Müller <muellerk2006@gmail.com>
2026-02-13 11:16:52 +0100

    Create function to print space

--- courses/coding-in-C/Lab_5/my_solution/section_3.c
+++ courses/coding-in-C/Lab_5/my_solution/section_3.c
@@ -6,6 +6,20 @@ void simulate_timestep(int* space, int* next, int size) {
     // TODO: Process this
 }
 
+void print_space(int *p_space, int size) {
+    printf("i: ");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", i);
+    }
+
+    printf("\n%3s", "");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", p_space[i]);
+    }
+
+    printf("\n");
+}
+
 int main(void) {
     int space[SPACE_SIZE] = { 0 };
 
@@ -15,6 +29,12 @@ int main(void) {
 
     int *p_space = space;
     int space_next[SPACE_SIZE] = { 0 };
+
+    printf("T = 0\n");
+    print_space(space, SPACE_SIZE);
+
+    printf("\nT = 1\n");
+    print_space(space_next, SPACE_SIZE);
 
     return 0;
 }
