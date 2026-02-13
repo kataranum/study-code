8047b8a601fe364ca1529d486ca70c72ab8c160f
K Müller <muellerk2006@gmail.com>
2026-02-13 11:13:19 +0100

    Create arrays for the space

--- courses/coding-in-C/Lab_5/my_solution/section_3.c
+++ courses/coding-in-C/Lab_5/my_solution/section_3.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 
+#define SPACE_SIZE 10
+
+void simulate_timestep(int* space, int* next, int size) {
+    // TODO: Process this
+}
+
 int main(void) {
+    int space[SPACE_SIZE] = { 0 };
+
+    space[2] = 1;
+    space[4] = 1;
+    space[6] = 1;
+
+    int *p_space = space;
+    int space_next[SPACE_SIZE] = { 0 };
+
     return 0;
 }
