83942e46eae290085b993682690151a141f4d9b6
K Müller <muellerk2006@gmail.com>
2026-02-13 10:57:39 +0100

    Expect at least one parameter

--- courses/coding-in-C/Lab_5/my_solution/section_2.c
+++ courses/coding-in-C/Lab_5/my_solution/section_2.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
 int main(int argc, char **argv) {
+    if (argc < 2) {
+        printf("Please input at least one parameter.\n");
+        return 1;
+    }
+
+    printf("argc: %d\n", argc);
+
     return 0;
 }
