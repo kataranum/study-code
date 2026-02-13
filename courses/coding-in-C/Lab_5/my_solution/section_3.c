13c84947d17307dd93a694cd08e08e11d2725940
K Müller <muellerk2006@gmail.com>
2026-02-13 11:25:54 +0100

    Make particle system run until anihilation

--- courses/coding-in-C/Lab_5/my_solution/section_3.c
+++ courses/coding-in-C/Lab_5/my_solution/section_3.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 #define SPACE_SIZE 10
 
@@ -13,6 +14,20 @@ void simulate_timestep(const int *p_space, int *p_next, int size) {
             p_next[i + 1] = particle;
         }
     }
+}
+
+int amount_particles(const int *p_space, int size) {
+    int amount_particles = 0;
+
+    for (int i = 0; i < size; i++) {
+        int particle = p_space[i];
+
+        if (particle) {
+            amount_particles++;
+        }
+    }
+
+    return amount_particles;
 }
 
 void print_space(const int *p_space, int size) {
@@ -31,21 +46,29 @@ void print_space(const int *p_space, int size) {
 
 int main(void) {
     int space[SPACE_SIZE] = { 0 };
+    int *p_space = space;
 
     space[2] = 1;
     space[4] = 1;
     space[6] = 1;
 
-    int *p_space = space;
-    int space_next[SPACE_SIZE] = { 0 };
+    int timestep = 0;
 
-    simulate_timestep(p_space, space_next, SPACE_SIZE);
+    while (true) {
+        printf("T = %d\n", timestep++);
+        print_space(space, SPACE_SIZE);
 
-    printf("T = 0\n");
-    print_space(space, SPACE_SIZE);
+        int space_next[SPACE_SIZE] = { 0 };
+        simulate_timestep(p_space, space_next, SPACE_SIZE);
 
-    printf("\nT = 1\n");
-    print_space(space_next, SPACE_SIZE);
+        memcpy(space, space_next, sizeof(space));
+
+        if (amount_particles(space, SPACE_SIZE) < 2) {
+            break;
+        }
+    }
+
+    printf("finished\n");
 
     return 0;
 }
