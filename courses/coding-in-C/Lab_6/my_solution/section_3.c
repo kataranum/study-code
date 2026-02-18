7f7b06fe39104c7c61e4b14c9eee5b6947dfd868
K Müller <muellerk2006@gmail.com>
2026-02-18 13:53:24 +0100

    Create structs SensorData and Sensor

--- courses/coding-in-C/Lab_6/my_solution/section_3.c
+++ courses/coding-in-C/Lab_6/my_solution/section_3.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 
+#define DATA_SIZE 3000
+
+typedef struct {
+    float time;
+    double probability;
+} SensorData;
+
+typedef struct {
+    int id;
+    double threshold;
+    SensorData data[DATA_SIZE];
+    int object_detection[DATA_SIZE];
+} Sensor;
+
 int main(void) {
     return 0;
 }
