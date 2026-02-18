e3938c914efde613990d4d0a430909eca755dd6e
K Müller <muellerk2006@gmail.com>
2026-02-18 13:15:43 +0100

    Define functions for reading and writing tests

--- courses/coding-in-C/Lab_6/my_solution/section_1.c
+++ courses/coding-in-C/Lab_6/my_solution/section_1.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
 
+#define INPUT_PATH "input.txt"
+#define OUTPUT_PATH "output.txt"
+
+void test_reading(void) {
+    FILE *f = fopen(INPUT_PATH);
+
+    fclose(f);
+}
+
+void test_writing(void) {
+    FILE *f = fopen(OUTPUT_PATH);
+
+    fclose(f);
+}
+
 int main(void) {
+    test_reading();
+    test_writing();
+
     return 0;
 }
