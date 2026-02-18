8feb7d825dceef5de2344395af89f1419553b8e7
K Müller <muellerk2006@gmail.com>
2026-02-18 15:35:42 +0100

    Implement read_sensor()

--- courses/coding-in-C/Lab_6/my_solution/section_3.c
+++ courses/coding-in-C/Lab_6/my_solution/section_3.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 #define DATA_SIZE 3000
 
@@ -30,9 +31,43 @@ Sensor init_sensor(int id, double threshold) {
     return sensor;
 }
 
+bool read_sensor(Sensor *p_sensor, const char *path) {
+    FILE *f = fopen(path, "r");
+
+    SensorData *p_data = p_sensor->data;
+
+    while (true) {
+        float seconds, measurement;
+
+        int res = fscanf(f, "%f %f\n", &seconds, &measurement);
+
+        if (res < 2) {
+            break;
+        }
+
+        p_data->time = seconds;
+        p_data->probability = measurement;
+
+        p_data++;
+    }
+
+    fclose(f);
+    return true;
+}
+
 int main(void) {
     Sensor sensor_1 = init_sensor(1, 0.8);
     Sensor sensor_2 = init_sensor(2, 0.7);
+
+    read_sensor(&sensor_1, "../sensor1.txt");
+    read_sensor(&sensor_2, "../sensor2.txt");
+
+    // debug print first 10 elements
+    for (int i = 0; i < 10; i++) {
+        printf("Sensor 1 data[%d] = %f; %f\n", i, sensor_1.data[i].time, sensor_1.data[i].probability);
+        printf("Sensor 2 data[%d] = %f; %f\n", i, sensor_2.data[i].time, sensor_2.data[i].probability);
+        printf("\n");
+    }
     
     return 0;
 }
