fba8c586444a81f3e0127cf16fe24325644de31c
K Müller <muellerk2006@gmail.com>
2026-02-18 13:44:06 +0100

    Init car and refuel

--- courses/coding-in-C/Lab_6/my_solution/section_2.c
+++ courses/coding-in-C/Lab_6/my_solution/section_2.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 typedef struct {
     float fuel_level,
@@ -15,6 +16,21 @@ int refuel(Car *p_car, float amount) {
     return 1;
 }
 
+Car init_car(float fuel_level, float max_fuel_level, const char *str_model) {
+    Car car;
+
+    car.fuel_level = fuel_level;
+    car.max_fuel_level = max_fuel_level;
+    strncpy(str_model, car.model, sizeof(car.model));
+
+    return car;
+}
+
 int main(void) {
+    Car car = init_car(10.0, 50.0, "Hyundai i10");
+
+    refuel(&car, 30.0);
+    refuel(&car, 50.0);
+
     return 0;
 }
