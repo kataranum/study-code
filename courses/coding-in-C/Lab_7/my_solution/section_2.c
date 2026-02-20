0aee40fcd84f1cd53fcc941a7b16797444413f6f
K Müller <muellerk2006@gmail.com>
2026-02-20 13:12:04 +0100

    Implement functions to append to list

--- courses/coding-in-C/Lab_7/my_solution/section_2.c
+++ courses/coding-in-C/Lab_7/my_solution/section_2.c
@@ -1,9 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 struct Node {
     double value;
     struct Node *p_next;
 } typedef Node;
+
+Node* ll_find_tail(Node *p_node) {
+    while (1) {
+        if (p_node->p_next == NULL) {
+            return p_node;
+        }
+
+        p_node = p_node->p_next;
+    }
+}
+
+Node* ll_append(double value, Node *p_head) {
+    Node *p_tail = ll_find_tail(p_head);
+
+    Node *p_new = malloc(sizeof(Node));
+    p_new->value = value;
+    p_new->p_next = NULL;
+
+    p_tail->p_next = p_new;
+
+    return p_new;
+}
 
 int main(void) {
     return 0;
