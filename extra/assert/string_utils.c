5be82e1ff64e5bd57f3cfdcbcea4f8d2fdc3a6dc
K Müller <muellerk2006@gmail.com>
2026-02-25 16:03:46 +0100

    Implement read_line_dynamic()

--- extra/assert/string_utils.c
+++ extra/assert/string_utils.c
@@ -1,4 +1,7 @@
 #include "string_utils.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int custom_len(const char *str) {
     int len = 0;
@@ -9,4 +12,29 @@ int custom_len(const char *str) {
     }
 
     return len;
+}
+
+char* read_line_dynamic() {
+    char buffer[256];
+
+    char *success = fgets(buffer, sizeof(buffer), stdin);
+
+    if (!success) {
+        return NULL;
+    }
+
+    // Replace newline with 0
+    int len = custom_len(buffer);
+    success[len - 1] = '\0';
+    len--;
+
+    char *p_str = malloc(sizeof(char) * len);
+
+    if (!p_str) {
+        return NULL;
+    }
+
+    memcpy(p_str, success, sizeof(char) * len);
+
+    return p_str;
 }
