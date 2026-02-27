337b8e8d07143041930c0f26d1f80626eacae985
K Müller <muellerk2006@gmail.com>
2026-02-27 14:14:11 +0100

    Create function headers in playlist.h

--- courses/coding-in-C/Lab_8/my_solution/include/playlist.h
+++ courses/coding-in-C/Lab_8/my_solution/include/playlist.h
@@ -17,4 +17,12 @@ typedef struct {
     Song *p_first;
 } Playlist;
 
+Playlist init_playlist(void);
+void delete_playlist(Playlist *p_playlist);
+
+void add_song(Playlist *p_playlist, const char *str_title, const char *str_artist);
+void delete_firstSong(Playlist *p_playlist);
+
+void print_playlist(Playlist playlist);
+
 #endif
