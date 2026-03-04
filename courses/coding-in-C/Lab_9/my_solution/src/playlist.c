6d38362aa26a6cd06655c24b45faa443680ab2c6
K Müller <muellerk2006@gmail.com>
2026-03-04 15:09:48 +0100

    Copy project from Lab 8

---
+++ courses/coding-in-C/Lab_9/my_solution/src/playlist.c
@@ -0,0 +1,119 @@
+#include <playlist.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// -------------------
+//  PRIVATE FUNCTIONS
+// -------------------
+
+/**
+ * @brief Allocate a new string on the heap
+ * 
+ * @param  str Content of new string
+ * @return     Pointer to new string
+ */
+char* alloc_string(const char *str) {
+    char *ptr = malloc(strlen(str) + 1);
+    if (ptr == NULL) {
+        printf("malloc failure\n");
+        exit(1);
+    }
+
+    strcpy(ptr, str);
+    return ptr;
+}
+
+/**
+ * @brief Free memory of an allocated `Song` struct
+ *
+ * WARNING: `Song` is supposed to be a part of a linked list, this function does
+ * not alter anything of this list, so just calling this function on a list
+ * entry will mess up the list.
+ * 
+ * @param p_song Pointer to `Song` to delete
+ */
+void delete_song(Song *p_song) {
+    free(p_song->p_title);
+    free(p_song->p_artist);
+    free(p_song);
+}
+
+// ------------------
+//  PUBLIC FUNCTIONS
+// ------------------
+
+Playlist init_playlist(void) {
+    Playlist playlist;
+    playlist.p_first = NULL;
+    playlist.length = 0;
+
+    return playlist;
+}
+
+void delete_playlist(Playlist *p_playlist) {
+    Song *p_song = p_playlist->p_first;
+
+    while (p_song != NULL) {
+        Song *p_next = p_song->p_next;
+        delete_song(p_song);
+        p_song = p_next;
+    }
+
+    p_playlist->p_first = NULL;
+    p_playlist->length = 0;
+}
+
+void add_song(Playlist *p_playlist, const char *str_title, const char *str_artist) {
+    if (p_playlist->length >= MAX_SONGS) {
+        printf("Cannot add more songs to playlist.\n");
+        return;
+    }
+
+    char *p_title = alloc_string(str_title);
+    char *p_artist = alloc_string(str_artist);
+
+    Song *p_new = malloc(sizeof(Song));
+    if (p_new == NULL) {
+        printf("malloc failure\n");
+        exit(1);
+    }
+    p_new->p_title = p_title;
+    p_new->p_artist = p_artist;
+    p_new->p_next = NULL;
+
+    p_playlist->length++;
+
+    if (p_playlist->p_first == NULL) {
+        p_playlist->p_first = p_new;
+        return;
+    }
+
+    Song *p_last = p_playlist->p_first;
+    while (p_last->p_next != NULL) {
+        p_last = p_last->p_next;
+    }
+
+    p_last->p_next = p_new;
+}
+
+void delete_firstSong(Playlist *p_playlist) {
+    if (p_playlist->p_first == NULL) {
+        return;
+    }
+
+    Song *p_second = p_playlist->p_first->p_next;
+    delete_song(p_playlist->p_first);
+    p_playlist->p_first = p_second;
+    p_playlist->length--;
+}
+
+void print_playlist(Playlist playlist) {
+    Song *p_song = playlist.p_first;
+
+    while (p_song != NULL) {
+        printf("Title: %s, Artist: %s\n", p_song->p_title, p_song->p_artist);
+
+        p_song = p_song->p_next;
+    }
+}
---
+++ courses/coding-in-C/Lab_9/my_solution/test/test_playlist.c
@@ -0,0 +1,168 @@
+#include <assert.h>
+#include <string.h>
+#include <stdio.h>
+#include <playlist.h>
+
+/* === Unit-tests === */
+
+// example unit test
+void test_init_playlist(void)
+{
+    Playlist p = init_playlist();
+
+    assert(p.p_first == NULL);
+    assert(p.length == 0);
+
+    delete_playlist(&p);
+
+    printf("test_init_playlist() passed\n");
+}
+
+void test_add_song(void)
+{
+    Playlist p = init_playlist();
+
+    add_song(&p, "Song title", "Song artist");
+    assert(p.length == 1);
+
+    add_song(&p, "Amazing\nMulti\nSong\n", "Amazing\nPerson");
+    assert(p.length == 2);
+
+    add_song(&p, "", "");
+    assert(p.length == 3);
+
+    Song *p_song = p.p_first;
+    assert(strcmp(p_song->p_title, "Song title") == 0);
+    assert(strcmp(p_song->p_artist, "Song artist") == 0);
+
+    p_song = p_song->p_next;
+    assert(strcmp(p_song->p_title, "Amazing\nMulti\nSong\n") == 0);
+    assert(strcmp(p_song->p_artist, "Amazing\nPerson") == 0);
+
+    p_song = p_song->p_next;
+    assert(strcmp(p_song->p_title, "") == 0);
+    assert(strcmp(p_song->p_artist, "") == 0);
+
+    assert(p_song->p_next == NULL);
+
+    delete_playlist(&p);
+
+    printf("test_add_song() passed\n");
+}
+
+void test_delete_firstSong(void)
+{
+    Playlist p = init_playlist();
+
+    add_song(&p, "1", "artist1");
+    add_song(&p, "2", "artist2");
+
+    Song *p_song = p.p_first;
+    assert(strcmp(p_song->p_title, "1") == 0);
+    assert(p.length == 2);
+
+    delete_firstSong(&p);
+    p_song = p.p_first;
+    assert(strcmp(p_song->p_title, "2") == 0);
+    assert(p.length == 1);
+
+    delete_firstSong(&p);
+    p_song = p.p_first;
+    assert(p_song == NULL);
+    assert(p.length == 0);
+
+    delete_playlist(&p);
+
+    printf("test_delete_firstSong() passed\n");
+}
+
+void test_delete_firstSong_empty(void)
+{
+    Playlist p = init_playlist();
+
+    // "Delete" first song in an empty playlist
+    delete_firstSong(&p);
+
+    assert(p.p_first == NULL);
+    assert(p.length == 0);
+
+    delete_playlist(&p);
+
+    printf("test_delete_firstSong_empty() passed\n");
+}
+
+void test_delete_playlist(void)
+{
+    Playlist p = init_playlist();
+
+    add_song(&p, "first song", "first artist");
+    add_song(&p, "second song", "second artist");
+
+    delete_playlist(&p);
+    assert(p.p_first == NULL);
+    assert(p.length == 0);
+
+    // Deleting an already "deleted" playlist should do nothing
+    delete_playlist(&p);
+    assert(p.p_first == NULL);
+    assert(p.length == 0);
+
+    printf("test_delete_playlist() passed\n");
+}
+
+void test_max_songs_limit() {
+    Playlist p = init_playlist();
+
+    // Fill playlist to max
+    for (int i = 0; i < MAX_SONGS; i++) {
+        char str_title[16];
+        char str_artist[16];
+
+        sprintf(str_title, "song %d", i);
+        sprintf(str_artist, "artist %d", i);
+
+        add_song(&p, str_title, str_artist);
+    }
+
+    // store expected title and artist of last song
+    char expected_title[16];
+    char expected_artist[16];
+
+    sprintf(expected_title, "song %d", MAX_SONGS - 1);
+    sprintf(expected_artist, "artist %d", MAX_SONGS - 1);
+
+    // find last song
+    Song *p_last = p.p_first;
+    while (p_last->p_next != NULL) {
+        p_last = p_last->p_next;
+    }
+
+    // do assertions
+    assert(strcmp(p_last->p_title, expected_title) == 0);
+    assert(strcmp(p_last->p_artist, expected_artist) == 0);
+    assert(p.length == MAX_SONGS);
+
+    // try adding another song, shouldn't add
+    add_song(&p, "unnecessary song", "i really don't want to be here");
+    assert(p_last->p_next == NULL);
+    assert(p.length == MAX_SONGS);
+
+    delete_playlist(&p);
+
+    printf("test_max_songs_limit() passed\n");
+}
+
+/* === Test-Runner === */
+
+int main(void)
+{
+    test_init_playlist();
+    test_add_song();
+    test_delete_firstSong();
+    test_delete_firstSong_empty(); // what happens if we delete first song from empty playlist
+    test_delete_playlist();
+    test_max_songs_limit(); // verify if the limit will be not be surpassed
+
+    printf("Alle Playlist-Tests erfolgreich bestanden.\n");
+    return 0;
+}
