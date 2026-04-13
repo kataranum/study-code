577084cc0731824413ff823bab3336e79c497619
K Müller <muellerk2006@gmail.com>
2026-04-13 14:01:45 +0200

    Implement all BankAccount functions

---
+++ courses/coding-in-C++/Lab_2/my_solution/bank_account.cpp
@@ -0,0 +1,25 @@
+#include "bank_account.hpp"
+
+void BankAccount::setOwner(const std::string& name) {
+    this->info.owner = name;
+}
+
+std::string BankAccount::get_owner() {
+    return this->info.owner;
+}
+
+void BankAccount::deposit(double amount) {
+    this->info.balance += amount;
+}
+
+void BankAccount::withdraw(double amount) {
+    this->info.balance -= amount;
+}
+
+double BankAccount::getBalance() {
+    return this->info.balance;
+}
+
+AccountInfo BankAccount::getAccountInfo() {
+    return this->info;
+}
