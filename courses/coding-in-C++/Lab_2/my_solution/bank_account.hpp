c174ae0fe7a8442c8a9ec62820fad87c8a239c91
K Müller <muellerk2006@gmail.com>
2026-04-13 13:58:51 +0200

    Move bank account class to new header file

---
+++ courses/coding-in-C++/Lab_2/my_solution/bank_account.hpp
@@ -0,0 +1,26 @@
+#ifndef H_BANKACCOUNT
+#define H_BANKACCOUNT
+
+#include <string>
+
+struct AccountInfo {
+    std::string owner;
+    double balance;
+};
+
+class BankAccount {
+private:
+    AccountInfo info;
+
+public:
+    void setOwner(const std::string& name);
+    std::string get_owner();
+
+    void deposit(double amount);
+    void withdraw(double amount);
+
+    double getBalance();
+    AccountInfo getAccountInfo();
+};
+
+#endif
--- courses/coding-in-C++/Lab_2/my_solution/main.cpp
+++ courses/coding-in-C++/Lab_2/my_solution/main.cpp
@@ -1,25 +1,5 @@
 #include <iostream>
 
-struct AccountInfo {
-    std::string owner;
-    double balance;
-};
-
-class BankAccount {
-private:
-    AccountInfo info;
-
-public:
-    void setOwner(const std::string& name);
-    std::string get_owner();
-
-    void deposit(double amount);
-    void withdraw(double amount);
-
-    double getBalance();
-    AccountInfo getAccountInfo();
-};
-
 int main(void) {
     std::cout << "hello" << std::endl;
 
