b65194e88ba918583bc986415addf8d79b805b06
K Müller <muellerk2006@gmail.com>
2026-04-13 13:57:38 +0200

    Define class BankAccount

--- courses/coding-in-C++/Lab_2/my_solution/main.cpp
+++ courses/coding-in-C++/Lab_2/my_solution/main.cpp
@@ -1,11 +1,27 @@
 #include <iostream>
 
-class BankAccount {
+struct AccountInfo {
+    std::string owner;
+    double balance;
+};
 
+class BankAccount {
+private:
+    AccountInfo info;
+
+public:
+    void setOwner(const std::string& name);
+    std::string get_owner();
+
+    void deposit(double amount);
+    void withdraw(double amount);
+
+    double getBalance();
+    AccountInfo getAccountInfo();
 };
 
 int main(void) {
     std::cout << "hello" << std::endl;
-    
+
     return 0;
 }
