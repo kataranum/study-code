7c33f0747dab252d54ccca4e35cd76ce781e183d
K Müller <muellerk2006@gmail.com>
2026-04-16 11:12:57 +0200

    Define read_student_data

--- courses/coding-in-C++/Lab_1/my_solution/student.cpp
+++ courses/coding-in-C++/Lab_1/my_solution/student.cpp
@@ -1,4 +1,15 @@
 #include <iostream>
+#include <string>
+#include <cstdint>
+
+void read_student_data(
+    std::string& name,
+    uint8_t& homework,
+    uint8_t& midterm,
+    uint8_t& finalExam
+) {
+
+}
 
 int main(void) {
     return 0;
