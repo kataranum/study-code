d96b028463b713c20769b82daa866e6ed660eefd
K Müller <muellerk2006@gmail.com>
2026-04-16 10:40:58 +0200

    Implement is_adult() and is_senior()

--- courses/coding-in-C++/Lab_1/my_solution/age_validation.cpp
+++ courses/coding-in-C++/Lab_1/my_solution/age_validation.cpp
@@ -1,4 +1,13 @@
 #include <iostream>
+#include <cstdint>
+
+bool is_adult(uint32_t age) {
+    return age >= 18;
+}
+
+bool is_senior(uint32_t age) {
+    return age >= 65;
+}
 
 int main(void) {
     std::cout << "Hello world" << std::endl;
