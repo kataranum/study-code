b0591c0e50ad1af09c0aeeb69f3bb910119ccf8f
K Müller <muellerk2006@gmail.com>
2026-04-23 14:33:47 +0200

    Fix SearchQuery constructor

--- courses/coding-in-C++/Lab_2/my_solution/section_VII/search_query.cpp
+++ courses/coding-in-C++/Lab_2/my_solution/section_VII/search_query.cpp
@@ -4,9 +4,8 @@
 SearchQuery::SearchQuery(const std::string& input, uint64_t max_results)
 : input(std::string(input)), max_results(max_results) { }
 
-SearchQuery::SearchQuery(const std::string& input) {
-    SearchQuery(input, UINT64_MAX);
-}
+SearchQuery::SearchQuery(const std::string& input)
+: SearchQuery(input, UINT64_MAX) { }
 
 bool SearchQuery::is_valid() const {
     if (this->input.empty()) {
