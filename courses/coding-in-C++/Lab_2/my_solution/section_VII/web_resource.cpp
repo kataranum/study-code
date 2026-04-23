a24f96b140d32092a0f15bb7c7a2007d81505ee4
K Müller <muellerk2006@gmail.com>
2026-04-23 11:12:12 +0200

    Implement getters for WebResource

--- courses/coding-in-C++/Lab_2/my_solution/section_VII/web_resource.cpp
+++ courses/coding-in-C++/Lab_2/my_solution/section_VII/web_resource.cpp
@@ -5,3 +5,15 @@ WebResource::WebResource(const std::string& address, const std::string& content)
     content(std::string(content)),
     views(0)
 { }
+
+const std::string& WebResource::get_address() const {
+    return this->address;
+}
+
+const std::string& WebResource::get_content() const {
+    return this->content;
+}
+
+uint64_t WebResource::get_views() const {
+    return this->views;
+}
