ad7cb049bc6db148ace275410b7f06ec5d794d29
K Müller <muellerk2006@gmail.com>
2026-04-23 12:58:28 +0200

    Implement querry functions for SearchEngine

--- courses/coding-in-C++/Lab_2/my_solution/section_VII/search_engine.cpp
+++ courses/coding-in-C++/Lab_2/my_solution/section_VII/search_engine.cpp
@@ -5,3 +5,15 @@ SearchEngine::SearchEngine() :
     database(std::vector<WebResource>()),
     total_queries(0)
 { }
+
+size_t SearchEngine::get_amount_resources() const {
+    return this->database.size();
+}
+
+uint64_t SearchEngine::get_total_queries() const {
+    return this->total_queries;
+}
+
+const WebResource& SearchEngine::fetch_result(const SearchResult& result) const {
+    return this->database[result.web_index];
+}
