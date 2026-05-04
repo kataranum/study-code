05cbd4c408543b52dd14067008d749b0e80e07cd
K Müller <muellerk2006@gmail.com>
2026-05-04 13:33:45 +0200

    Define Vector2D class

---
+++ courses/coding-in-C++/Lab_4/my_solution/section1/vector2d.hpp
@@ -0,0 +1,16 @@
+#ifndef VECTOR2D_H
+#define VECTOR2D_H
+
+class Vector2D {
+public:
+    double x;
+    double y;
+
+public:
+    Vector2D();
+    Vector2D(double x, double y);
+
+    void print();
+};
+
+#endif
